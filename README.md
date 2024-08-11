1964137477b8fe8854d7bb4b96c98f5159f672eb
franklinosei <franklinosei15@gmail.com>
2024-08-11 15:38:39 +0000

    Add dvc, ReadMe and experimentation notebook

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# palm-trees-counter
+# Palm Tree Detection and Counting
+
+## Project Overview
+
+This project focuses on detecting and counting palm trees in images using deep learning techniques. It leverages a pre-trained model, `fasterrcnn_resnet50_fpn`, and fine-tunes it on a custom dataset with annotations provided in a CSV file format. The project also incorporates MLOps practices, including model versioning, data tracking, and performance monitoring, using tools like Docker and MLflow.
+
+## Table of Contents
+1. [Project Overview](#project-overview)
+
+## Features
+
+- **Object Detection**: Detects and counts palm trees in images using a fine-tuned Faster R-CNN model.
+- **Model Performance Metrics**: Calculates Mean Absolute Error (MAE) and Root Mean Squared Error (RMSE) to evaluate counting accuracy.
+- **Image Preprocessing**: Automatically resizes images to 512x512 and normalizes bounding box coordinates.
+- **Visualization**: Displays annotated images with bounding boxes and class labels.
+- **API Integration**: Serves the model via a FastAPI application.
+- **MLOps Integration**: Uses Docker for containerization and MLflow for experiment tracking and model management.
--- src/palmTreesCounter/config/configuration.py
+++ src/palmTreesCounter/config/configuration.py
@@ -2,21 +2,44 @@ from palmTreesCounter.constants import *
 from palmTreesCounter.utils.common import read_yaml, create_directories
 from palmTreesCounter.entity.config_entity import (DataIngestionConfig)
 
-
 class ConfigurationManager:
+    """
+    Class to manage the configuration of the application.
+
+    It reads configuration parameters from YAML files and provides methods to access them.
+
+    Attributes:
+        config (dict): Dictionary containing configuration parameters.
+        params (dict): Dictionary containing model parameters.
+    """
     def __init__(
         self,
         config_filepath = CONFIG_FILE_PATH,
         params_filepath = PARAMS_FILE_PATH):
+        """
+        Initializes the ConfigurationManager with configuration and parameters file paths.
 
+        Args:
+            config_filepath (str, optional): Path to the configuration YAML file. 
+                                              Defaults to CONFIG_FILE_PATH.
+            params_filepath (str, optional): Path to the parameters YAML file. 
+                                             Defaults to PARAMS_FILE_PATH.
+        """
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
-        config = self.config.data_ingestion
+        """
+        Returns a DataIngestionConfig object containing the configuration 
+        parameters for data ingestion.
 
+        Returns:
+            DataIngestionConfig: Object with data ingestion configuration parameters.
+        """
+
+        config = self.config.data_ingestion
         create_directories([config.root_dir])
 
         data_ingestion_config = DataIngestionConfig(
