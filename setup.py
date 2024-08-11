b6a43bcacdb61c5bc286a38f57bb9effd3ef212a
franklinosei <franklinosei15@gmail.com>
2024-08-11 13:28:21 +0000

    Add setup and config

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="An object detection python package for counting the number of palm trees",
+    description="An object detection python package for counting the number of palm trees in an image.",
     long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
